616a54d82269016d9e7739ffd29b883aca33c07d
Advay Patil <advaypatil27@gmail.com>
2023-05-30 00:26:44 -0700

    load fonts in

--- App.js
+++ App.js
@@ -6,12 +6,26 @@ import "react-native-gesture-handler";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import HomeScreen from "./app/screens/HomeScreen";
 import { useFonts } from "expo-font";
+import {
+  Montserrat_100Thin,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_900Black,
+} from "@expo-google-fonts/montserrat";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   const fontsLoaded = useFonts({
     Ionicons: require("./assets/Fonts/Ionicons.ttf"),
+    Thin: Montserrat_100Thin,
+    Light: Montserrat_300Light,
+    Regular: Montserrat_400Regular,
+    Semibold: Montserrat_600SemiBold,
+    Bold: Montserrat_700Bold,
+    Black: Montserrat_900Black,
   });
 
   if (!fontsLoaded) return null;
