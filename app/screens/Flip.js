e9fe336541df4973418b8c52f364c9b8c87ef2c5
Advay Patil <advaypatil27@gmail.com>
2023-06-26 07:19:00 -0700

    style the flip page

--- app/screens/Flip.js
+++ app/screens/Flip.js
@@ -1,12 +1,47 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { SafeAreaView, Text, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
 import { colors } from "../config/colors";
+import Icon from "react-native-vector-icons/Ionicons";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { TouchableOpacity } from "react-native";
 
 export default function Flip() {
+  const terms = ["asdf", "asd", "asdff", "dd", "asf", "as"];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const updateIndex = (amount) => {
+    setCurrentIndex(currentIndex + amount);
+    if (amount < 0) {
+      setCurrentIndex(Math.max(currentIndex, 0));
+      return;
+    }
+    setCurrentIndex(Math.min(currentIndex, terms.length));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Flip</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.termContainer}>
+        <Text style={styles.text}>{terms[currentIndex]}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            setCurrentIndex(Math.max(currentIndex - 1, 0));
+            console.log(currentIndex);
+          }}
+        >
+          <Icon name="caret-back-circle-outline" size={80} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setCurrentIndex(Math.min(currentIndex + 1, terms.length - 1));
+          }}
+        >
+          <Icon name="caret-forward-circle-outline" size={80} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -22,6 +57,23 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 32,
     fontFamily: "Bold",
-    color: colors.tertiaryAccent,
+  },
+  termContainer: {
+    height: "40%",
+    width: "90%",
+    borderWidth: 1,
+    borderColor: colors.grayBorderColor,
+    borderRadius: 15,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-evenly",
+    marginTop: 40,
   },
 });
